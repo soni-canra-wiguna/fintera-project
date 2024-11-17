f67403874a7bb1ef038d4cab40188dc1da23702c
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-11-17 22:49:35 +0700

    WIP Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import NextTopLoader from "nextjs-toploader"
 import { Toaster } from "@/components/ui/toaster"
 import { MAIN_COLOR } from "@/constants/colors"
 import { WithChildren } from "@/types"
+import { GoogleAnalytics } from "@/components/analytics"
 
 export const metadata: Metadata = {
   title: {
@@ -117,6 +118,7 @@ export default function RootLayout({ children }: Readonly<WithChildren>) {
       <TanstackQueryProvider>
         <ReduxProvider>
           <html lang="en">
+            {/* <GoogleAnalytics /> */}
             <body className={plusJakartaSans.className}>
               <NextTopLoader color={MAIN_COLOR} height={3} showSpinner={false} />
               <ThemeProvider
