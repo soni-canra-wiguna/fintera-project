3b95126d02a8eeae12a48ad9b91782c685e59e4d
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-11-18 09:31:35 +0700

    feat(ui): replace icon & wrap with link

--- components/layout/top-bar.tsx
+++ components/layout/top-bar.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link"
 import { Container } from "./container"
 import { UserProfile } from "@/components/pages/dashboard/user-profile"
 import { SearchBar } from "@/components/pages/dashboard/search"
-import { Bell } from "lucide-react"
+import { CircleHelp } from "lucide-react"
 import { TopBarWrapper } from "./topbar-wrapper"
 import { auth } from "@clerk/nextjs/server"
 
@@ -14,7 +15,9 @@ export const TopBar = async () => {
       <Container className="flex items-center gap-6 py-3">
         <SearchBar token={token ?? ""} />
         <div className="flex items-center gap-6">
-          <Bell className="size-5 stroke-[1.5]" />
+          <Link href="/blog">
+            <CircleHelp className="size-5 stroke-[1.5]" />
+          </Link>
           <UserProfile userId={userId ?? ""} />
         </div>
       </Container>
